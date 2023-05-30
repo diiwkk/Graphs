c31298c7fa00c348d515402ba97fe9553bd8fd20
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-30 23:50:40 +0600

    a

--- src/Vertex.java
+++ src/Vertex.java
@@ -7,4 +7,18 @@ public class Vertex<V> {
     public V getData() {
         return data;
     }
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    public String toString() {
+        return String.valueOf(data);
+    }
+
+
 }
